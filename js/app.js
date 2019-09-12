c73b961b0eefe12881c59a956c171d8c75fa2fe7
medvedium <mumbles2606@gmail.com>
2019-09-12 10:47:17 +0300

    finish

--- js/app.js
+++ js/app.js
@@ -1,102 +1,18 @@
 $(function() {
-  $(".problems__list--item1").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-01").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-01").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item2").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-02").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-02").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item3").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-03").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-03").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item4").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-04").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-04").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item5").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-05").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-05").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item6").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-06").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-06").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item7").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-07").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-07").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item8").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-08").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-08").removeClass("problems__body--active");
-    }
-  );
-
-  $(".problems__list--item9").hover(
-    function() {
-      $(this).addClass("problems__list--active");
-      $(".problems__body--position-09").addClass("problems__body--active");
-    },
-    function() {
-      $(this).removeClass("problems__list--active");
-      $(".problems__body--position-09").removeClass("problems__body--active");
-    }
-  );
+  for (let i = 1; i < 10; i++) {
+    $(".problems__list--item" + i).hover(
+      function() {
+        $(this).addClass("problems__list--active");
+        $(".problems__body--position-0" + i).addClass("problems__body--active");
+      },
+      function() {
+        $(this).removeClass("problems__list--active");
+        $(".problems__body--position-0" + i).removeClass(
+          "problems__body--active"
+        );
+      }
+    );
+  }
 
   $(".reviews__switch--item").on("click", function() {
     $(".reviews__switch--item").toggleClass("active");
