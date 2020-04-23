29e62366b9e0da939172446cbb04c4d981046c99
medvedium <mumbles2606@gmail.com>
2020-04-23 23:05:36 +0300

    fixes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
-$(function() {
+$(function () {
   for (let i = 1; i < 10; i++) {
-    $(".problems__list--item" + i).hover(
-      function() {
-        $(this).addClass("problems__list--active");
-        $(".problems__body--position-0" + i).addClass("problems__body--active");
-      },
-      function() {
-        $(this).removeClass("problems__list--active");
-        $(".problems__body--position-0" + i).removeClass(
-          "problems__body--active"
-        );
-      }
-    );
+    $(".problems__list--item" + i).hover(function () {
+      $(this).toggleClass("problems__list--active");
+      $(".problems__body--position-0" + i).toggleClass(
+        "problems__body--active"
+      );
+    });
   }
 
-  $(".reviews__switch--item").on("click", function() {
+  $(".reviews__switch--item").on("click", function () {
     $(".reviews__switch--item").toggleClass("active");
     $(".reviews__inner").toggleClass("reviews__inner-active");
   });
